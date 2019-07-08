3fe6788b6e9db7a0014014cd96f49eb2a01f5c72
DaryaV@munich.quantag-it.com <darya.vyshnyakova@quantag-it.com>
2019-07-08 17:12:00 +0300

    Updates

---
+++ precache-manifest.660cfcef20bbecbfc7ec55d844234d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f039e08c977c4f635fe7ac3e867d10f6",
+    "url": "/rreact_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "8d6aab9868f066124cec",
+    "url": "/rreact_dynamic-list-of-posts/static/css/main.fe08cbad.chunk.css"
+  },
+  {
+    "revision": "193ad16df643c436a7bb",
+    "url": "/rreact_dynamic-list-of-posts/static/js/2.ae2e3e31.chunk.js"
+  },
+  {
+    "revision": "8d6aab9868f066124cec",
+    "url": "/rreact_dynamic-list-of-posts/static/js/main.6d8bd1e6.chunk.js"
+  },
+  {
+    "revision": "29182feab4557f0479b2",
+    "url": "/rreact_dynamic-list-of-posts/static/js/runtime~main.9d771937.js"
+  },
+  {
+    "revision": "0a4a76ac15b5e290ce01cdf3795a291d",
+    "url": "/rreact_dynamic-list-of-posts/static/media/main-bg.0a4a76ac.png"
+  }
+]);
